2dc8d78a0915df6e0de46e4ad15bdad53103518a
mohanreddy592 <mohanreddy592@gmail.com>
2017-12-11 12:32:55 +0530

    slide show

--- app.js
+++ app.js
@@ -27,4 +27,30 @@ myapp.controller('MyCtrl', function($scope){
         $scope.selectedStudent = student;
     };
     
+    $scope.imagesURLs = [
+        'https://www.jeep.com/content/dam/fca-brands/na/jeep/en_us/2018/compass-mp/gallery/interior/2018-Jeep-Compass-VLP-Gallery-Exterior-01.jpg.image.1440.jpg',
+        'https://auto.ndtvimg.com/car-images/gallery/jeep/compass/exterior/jeep-compass-rear-side-view.jpg?v=2017-07-31',
+        'https://www.jeep.com/content/dam/fca-brands/na/jeep/en_us/2017/compass-mp/gallery/exterior/2017-Jeep-Compass-VLP-Gallery-Trailhawk-Side-Profile.jpg.image.1440.jpg',
+        'http://cdn1.autoexpress.co.uk/sites/autoexpressuk/files/styles/article_main_image/public/2017/06/170606_jeep-compass_10.jpg?itok=7WRq_ZY5',
+        'http://cdn2.alphr.com/sites/alphr/files/styles/16x9_860/public/2017/06/jeep_compass_review_2017_main.jpg?itok=251syBTy'
+    ];
+    $scope.index = 0;
+    //$scope.firstImg = 'https://auto.ndtvimg.com/car-images/gallery/jeep/compass/exterior/jeep-compass-front-profile.jpg?v=2017-07-31';
+    //$scope.firstImg = imagesURLs[index];
+
+    $scope.increaseIndex = function() {
+        if($scope.index === $scope.imagesURLs.length-1) {
+            $scope.index = 0;
+        } else {
+            $scope.index = $scope.index + 1;        
+        }
+    }
+    
+    $scope.decreaseIndex = function() {
+        if($scope.index === 0) {
+            $scope.index = $scope.imagesURLs.length-1
+        } else {
+            $scope.index = $scope.index - 1;        
+        }
+    }
 });
